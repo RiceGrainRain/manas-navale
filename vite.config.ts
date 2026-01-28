5e522e7f7ce124fbb1057dc56993564a5acbe64b
RiceGrainRain <mnmanasnavale@gmail.com>
2026-01-27 21:39:04 -0600

    tailwind init

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -6,7 +7,9 @@ export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [
+          tailwindcss(),
+          ['babel-plugin-react-compiler']],
       },
     }),
   ],
